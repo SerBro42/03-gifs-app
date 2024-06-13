26653ff5cb375fe2e71297c907e3f610e271a442
Sergey Brovko <serbro7@gmail.com>
2024-06-13 12:59:58 +0200

    @ViewChild - Implementation for making a local reference

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,7 +1,9 @@
 
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
+/* keyup.enter significa que se dispara cada vez que se levanta la tecla Enter despuès de
+ser pulsada. #txtTagInput es como ponerle un ID */
 @Component({
   selector: 'gifs-search-box',
   template: `
@@ -9,11 +11,27 @@ import { Component } from '@angular/core';
     <input type="text"
       class="form-control"
       placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag(  )"
+      #txtTagInput
     >
   `
 })
 
 export class SearchBoxComponent {
+
+  /* Este ViewChild está haciendo referencia al elemento html que tiene la etiqueta txtTagInput.
+  Sirve para hacer una "referencia local".
+  Ponemos el non-null assertion operator en la declaración de tagInput, porque sale el error de
+  que no está inicializado */
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() { }
+
+  searchTag( ) {
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({ newTag });
+  }
 
 }
