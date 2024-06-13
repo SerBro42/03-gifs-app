c64e77cbb3c2c02620dc58a17a8ab7db412ac7cb
Sergey Brovko <serbro7@gmail.com>
2024-06-13 18:19:38 +0200

    input validation functions

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,9 +15,28 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  public searchTag( tag: string ): void {
+  private organiseHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if ( this._tagsHistory.includes(tag) ) {
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag );
+    }
 
     this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+
+  }
+
+  public searchTag( tag: string ): void {
+    /* Primer filtro. Si haces click en el input, no insertas nada y pulsas Enter, no sucede
+    nada */
+    if (tag.length === 0) return;
+    /* Segundo filtro. Creamos una función aparte para aplicar filtros adicionales y la llamamos
+    desde aquí */
+    this.organiseHistory(tag);
+
+    //this._tagsHistory.unshift( tag ); //Esta llamada a función es redundante
+    console.log(this._tagsHistory);
 
   }
 
