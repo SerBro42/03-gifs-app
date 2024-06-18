b2a419b8364f4fc5c5d762a27e0c1fb8e89bf4ca
Sergey Brovko <serbro7@gmail.com>
2024-06-18 10:19:56 +0200

    assets folder created and angular.json edited to implement it. Lazy image component: creation and implementation. Creation of loader.svg

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+
+  /* Ambos inputs reciben información del componente padre (gifs-card.component) */
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if( !this.url ) throw new Error('URL property is required');
+  }
+
+  onLoad() {
+    console.log('Image loaded');
+    this.hasLoaded = true;
+  }
+
+}
